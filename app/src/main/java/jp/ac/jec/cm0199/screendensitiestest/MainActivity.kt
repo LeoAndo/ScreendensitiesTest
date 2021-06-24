f35694b82d0afd8ac38ea7960101211cda1ceee0
Reo Ando <ando.reo420@gmail.com>
2021-06-24 19:08:19 +0900

    多言語対応

--- app/src/main/java/jp/ac/jec/cm0199/screendensitiestest/MainActivity.kt
+++ app/src/main/java/jp/ac/jec/cm0199/screendensitiestest/MainActivity.kt
@@ -6,38 +6,36 @@ import jp.ac.jec.cm0199.screendensitiestest.databinding.ActivityMainBinding
 import java.lang.IllegalArgumentException
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater).also {
-            setContentView(it.root)
+        ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+            // 画面のピクセル密度のスケールを取得する
+            val scale: Float = resources.displayMetrics.density
+            textDensity.text = getDisplayScaleText(scale)
+            imageView.setImageResource(R.drawable.ic_launcher)
         }
-        // 画面のピクセル密度のスケールを取得する
-        val scale: Float = resources.displayMetrics.density
-        binding.textDensity.text = getDisplayScaleText(scale)
-
-        binding.imageView.setImageResource(R.drawable.ic_launcher)
     }
 
     private fun getDisplayScaleText(scale: Float): String {
         return when {
             scale <= Density.LDPI.scale -> {
-                "$scale : " + Density.LDPI.message
+                getString(R.string.ldpi_message, scale)
             }
             scale <= Density.MDPI.scale -> {
-                "$scale : " + Density.MDPI.message
+                getString(R.string.mdpi_message)
             }
             scale <= Density.HDPI.scale -> {
-                "$scale : " + Density.HDPI.message
+                getString(R.string.hdpi_message)
             }
             scale <= Density.XHDPI.scale -> {
-                "$scale : " + Density.XHDPI.message
+                getString(R.string.xhdpi_message)
             }
             scale <= Density.XXHDPI.scale -> {
-                "$scale : " + Density.XXHDPI.message
+                getString(R.string.xxhdpi_message)
             }
             scale <= Density.XXXHDPI.scale -> {
-                "$scale : " + Density.XXXHDPI.message
+                getString(R.string.xxxhdpi_message)
             }
             else -> {
                 throw IllegalArgumentException("unknown...")
@@ -46,12 +44,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     // 解像度の種類.
-    enum class Density(val scale: Float, val message: String) {
-        LDPI(0.75F, "低密度(ldpi) 0.75倍"),
-        MDPI(1.0F, "中密度(mdpi) 1.0倍、基準"),
-        HDPI(1.5F, "高密度(hdpi) 1.5倍"),
-        XHDPI(2.0F, "超高密度(xhdpi) 2.0倍"),
-        XXHDPI(3.0F, "超超高密度(xxhdpi) 3.0倍"),
-        XXXHDPI(4.0F, "超超超高密度(xxxhdpi) 4.0倍"),
+    enum class Density(val scale: Float) {
+        LDPI(0.75F),
+        MDPI(1.0F),
+        HDPI(1.5F),
+        XHDPI(2.0F),
+        XXHDPI(3.0F),
+        XXXHDPI(4.0F),
     }
 }
